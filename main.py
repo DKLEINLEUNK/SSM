e1e14679c5cfd31ed47aebb0b7cee85e1943956a
DKLEINLEUNK <d.kleinleunk@gmail.com>
2023-10-23 04:29:33 +0200

    Defined SIRNetworkModel class

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import networkx as nx
 import ndlib.models.epidemics as ep
 import ndlib.models.ModelConfig as mc
 
 
 class SIRNetworkModel:
-    """Class for generating SIR network models.
-
-    Attributes
-    ----------
-    topology : str
-        The topology of the network model. The available topologies are:
-        - `small-world`: Watts–Strogatz graph.
-        - `random`: Erdős–Rényi graph.
-        - `scale-free`: Barabási–Albert graph.
-
-    y : list
-        The parameters for the network model, specified as a list. The required parameters for each topology are as follows:
-        - small-world: `[num_nodes, k, p]`
-        - random: `[num_nodes, p]`
-        - scale-free: `[num_nodes, m]`
-    """
+    """Class for generating SIR network models."""
 
     def __init__(self, topology: str, y: list):
-        """Initializes the SIR network model.
+        """Class for generating SIR network models.
 
-        Parameters
+        Attributes
         ----------
         topology : str
-            The topology of the network model. The available topologies are:
+            The topology of the network model. 
+            The available topologies are:
             - `small-world`: Watts–Strogatz graph.
             - `random`: Erdős–Rényi graph.
             - `scale-free`: Barabási–Albert graph.
 
         y : list
-            The parameters for the network model, specified as a list. The required parameters for each topology are as follows:
+            The parameters for the network model, 
+            specified as a list. 
+            The required parameters per topology are:
             - small-world: `[num_nodes, k, p]`
             - random: `[num_nodes, p]`
             - scale-free: `[num_nodes, m]`
@@ -100,33 +88,38 @@ class SIRNetworkModel:
         self.iters = self.model.iteration_bunch(iterations)
         self.trends = self.model.build_trends(self.iters)
 
+    def plot_network(self):
+        """Plots the network."""
+
+        plt.figure(figsize=(12, 10))
+        pos = nx.spring_layout(self.network)
+        EDGE_COLOR = "black"
+        EDGE_WIDTH = 0.01
+        nx.draw(
+            self.network,
+            pos,
+            node_size=10,
+            node_color="r",
+            with_labels=False,
+            edge_color=EDGE_COLOR,
+            width=EDGE_WIDTH,
+        )
+        plt.title(f"{self.topology}".capitalize(),
+                  fontsize=16, fontweight="bold")
+        plt.axis("off")
+        plt.show()
+
 
 ### Example Usage ###
 num_nodes = 1000
 p = 0.01
 SIR_network_model = SIRNetworkModel('random', [num_nodes, p])
 SIR_network_model.configure_SIR_model(beta=0.01, gamma=0.005, I=0.05)
-SIR_network_model.run_simulation(200)
-
-# Fetch the network and trends
 network = SIR_network_model.network
-iterations, trends = SIR_network_model.iters, SIR_network_model.trends
 
-# Plot the trends
-import matplotlib.pyplot as plt
-plt.figure(figsize=(12, 10))
-positioning = nx.spring_layout(network)
-EDGE_COLOR = "black"
-EDGE_WIDTH = 0.01
-nx.draw(
-    network,
-    positioning,
-    node_size=10,
-    node_color="r",
-    with_labels=False,
-    edge_color=EDGE_COLOR,
-    width=EDGE_WIDTH,
-)
-plt.title("Erdos-Renyi Network", fontsize=16, fontweight="bold")
-plt.axis("off")
-plt.show()
+# Plot the network
+SIR_network_model.plot_network()
+
+# Run the simulation
+SIR_network_model.run_simulation(200)
+iterations, trends = SIR_network_model.iters, SIR_network_model.trends

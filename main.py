28b6e9dc403ab28f233e7375d44b9384db989115
DKLEINLEUNK <d.kleinleunk@gmail.com>
2023-10-26 20:47:49 +0200

    Added and extended plotting functionality.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from ndlib.viz.mpl.DiffusionPrevalence import DiffusionPrevalence
 # from ndlib.viz.bokeh.DiffusionTrend import DiffusionTrend as BokehDiffusionTrend
 # from ndlib.viz.bokeh.MultiPlot import MultiPlot
 # import ndlib.models.epidemics as ep
+import numpy as np
+from scipy.stats import binom
 
 
 class SIRNetworkModel:
@@ -36,6 +38,7 @@ class SIRNetworkModel:
         # Set the network topology
         self.topology = topology
         self.y = y
+        self.num_nodes = y[0]
 
         if topology == 'small-world':
             num_nodes, k, p = y[0], y[1], y[2]
@@ -57,6 +60,7 @@ class SIRNetworkModel:
         self.iters = None
         self.trends = None
         self.node_degrees_freq = None
+        self.num_nodes = None
 
     def configure_SIR_model(self, beta: float, gamma: float, I: float):
         """Configures the SIR model on networks.
@@ -84,47 +88,91 @@ class SIRNetworkModel:
 
         self.model = model
 
-    def plot_degree_distribution(self, path: str = 'Plots/degree_distribution'):
-        """Plots the degree distribution of the network."""
-        # Fetch the degree distribution
-        self.node_degrees_freq = nx.degree_histogram(self.network)  # frequencies of each degree value, k
-        
-        # Plot
-        plt.figure(figsize=(12, 10))
-        plt.bar(range(len(self.node_degrees_freq)),
-                self.node_degrees_freq)
-        plt.xlabel('Degree')
-        plt.ylabel('Frequency')
-        plt.title(f'Degree distribution of {self.topology} network')
-        plt.savefig(f'{path}.png', dpi=300)
-
     def run_simulation(self, iterations: int):
         """Runs the simulation."""
         self.iters = self.model.iteration_bunch(iterations)
         self.trends = self.model.build_trends(self.iters)
 
-    def plot_network(self):
-        """Plots the network."""
+    ### Visualization ###
+    def plot_degree_distribution(self, expected=True, save=False):
+        """Plots the degree distribution of the network.
+        
+        Parameters
+        ----------
+        `expected` : bool
+            Whether to plot the expected degree distribution (default = True).
+        `save` : bool
+            Whether to save the plot (default = False).
+        """
+        if self.topology == 'random': self.plot_degree_distribution_random(expected, save)
+        else: pass # TODO add other topologies
+    
+    def plot_degree_distribution_random(self, expected=False, save=False):
+        """Plots the degree distribution of a random network.
+
+        Requires assumption of independent formation is met.
+        """
+        degree_freqs = nx.degree_histogram(self.network)  # frequencies of each degree value, k
+        degrees = np.arange(len(degree_freqs))
+        
+        fig, ax = plt.subplots()
+        # plt.figure(figsize=(12, 10)) # TODO add this back in
+        ax.bar(degrees, degree_freqs)
+        
+        if expected:
+            n = len(self.network)  # nodes
+            m = self.network.size()  # edges
+            p_k = 2 * m / (n * (n - 1))  # probability of edge formation, assumes independence
+            expected_freqs = [binom.pmf(k, n-1, p_k) * n for k in degrees]
+
+            ax.plot(degrees, expected_freqs, 'bo-', label='Expected')
+
+        ax.set_xlabel('Degree')
+        ax.set_ylabel('Frequency')
+
+        plt.title(f'Degree distribution of {self.topology} network')
+        plt.show()
+
+        if save:
+            plt.savefig('Plots/degree_distribution.png', dpi=400)
+
+    def plot_degree_distribution_loglog(self, save=False):
+        """Plots the degree distribution of the network on a log-log scale."""
+
+    def plot_network(self, save=False):
+        """Plots the network itself."""
         plt.figure(figsize=(12, 10))
-        pos = nx.spring_layout(self.network)
-        edge_color = "black"
-        edge_width = 0.01
+        pos = nx.kamada_kawai_layout(self.network)
+
+        # Set node sizes & colors (based on degree)
+        degrees = np.array([val for (node, val) in self.network.degree()])  # TODO: add to init
+        normalized_degrees = (degrees - min(degrees)) / (max(degrees) - min(degrees))
+        node_sizes = degrees * 15 + 10
+        node_colors = plt.cm.magma(normalized_degrees)  # fades from black to bright red
+        
+        # alpha = 0.9  # transparency
+
         nx.draw(
             self.network,
             pos,
-            node_size=10,
-            node_color="r",
+            node_size=node_sizes,
+            node_color=node_colors,
+            # alpha=alpha,
             with_labels=False,
-            edge_color=edge_color,
-            width=edge_width,
+            edge_color="#1F2022",  # "Rich Grey" from https://colorcodes.io/gray/rich-gray-color-codes/
+            edgecolors="black",  # note: this is the node border color (for some stupid reason...)
+            linewidths=0.5,
+            width=0.5,  # edge width
+            # edge_alpha=alpha  TODO: add this back in for nx.draw_networkx_edges
         )
-        plt.title(f"{self.topology}".capitalize(),
-                  fontsize=16, fontweight="bold")
+
+        plt.title(f"{self.topology} network".capitalize(), fontsize=16, fontweight="bold")
         plt.axis("off")
-        plt.savefig('Plots/network.png', dpi=300)
+        plt.show()
+        if save:
+            plt.savefig('Plots/network.png', dpi=400)
 
-
-    def plot_diffusion_trend(self, path: str = 'Plots/diffusion_trend'):
+    def plot_diffusion_trend(self, save=False):
         """Plots the diffusion trend and stores as `path`.png.
 
         As taken from `https://ndlib.readthedocs.io/en/latest/reference/viz/mpl/DiffusionTrend.html`:
@@ -133,9 +181,10 @@ class SIRNetworkModel:
         Each trend line describes the variation of the number of nodes for a given status iteration after iteration."
         """
         viz = DiffusionTrend(self.model, self.trends)
-        viz.plot(f'{path}.png')
+        if save:
+            viz.plot('Plots/diffusion_trend.png')
 
-    def plot_diffusion_prevalence(self, path: str = 'Plots/diffusion_prevalence'):
+    def plot_diffusion_prevalence(self, save=False):
         """Plots the diffusion prevalence and stores as `path`.png.
 
         As taken from `https://ndlib.readthedocs.io/en/latest/reference/viz/mpl/DiffusionPrevalence.html`:
@@ -144,9 +193,15 @@ class SIRNetworkModel:
         Each trend line describes the delta of the number of nodes for a given status iteration after iteration."
         """
         viz = DiffusionPrevalence(self.model, self.trends)
-        viz.plot(f'{path}.png')
+        if save:
+            viz.plot('Plots/diffusion_prevalence.png')
 
-    def get_centralities(self):
+    ### Metrics ###
+    def get_average_degree(self):
+        """Returns the average degree of the network."""
+        return np.mean([val for (node, val) in self.network.degree()])
+    
+    def get_centralities(self, verbose=False):
         """Returns the degree, betweenness, and closeness centralities of the network."""
         degree = nx.degree_centrality(self.network)
         betweenness= nx.betweenness_centrality(self.network)
@@ -156,10 +211,16 @@ class SIRNetworkModel:
         # pagerank = nx.pagerank(G)
         # hits = nx.hits(G)
 
+        if verbose:
+            print(f"Degree centrality: {degree}")
+            print(f"Betweenness centrality: {betweenness}")
+            print(f"Closeness centrality: {closeness}")
+
         return degree, betweenness, closeness
 
 ### Example Usage ###
-num_nodes = 1000
+np.random.seed(42069) # for reproducibility
+num_nodes = 750
 p = 0.01
 random_network_SIR = SIRNetworkModel('random', [num_nodes, p])
 random_network_SIR.configure_SIR_model(beta=0.01, gamma=0.005, I=0.05)
@@ -169,14 +230,15 @@ network = random_network_SIR.network
 # random_network_SIR.plot_network()
 
 # Run simulation
-# random_network_SIR.run_simulation(200)
+random_network_SIR.run_simulation(1000)
 
 # Plot diffusion trend and prevalence
-# random_network_SIR.plot_diffusion_trend()
-# random_network_SIR.plot_diffusion_prevalence()
+# random_network_SIR.plot_diffusion_trend(save=True)
+# random_network_SIR.plot_diffusion_prevalence(save=True)
 
 # Plot degree distribution
 # random_network_SIR.plot_degree_distribution()
+random_network_SIR.plot_degree_distribution(expected=True, save=False)
 
 # Get centralities
-# print(random_network_SIR.get_centralities())
+# random_network_SIR.get_centralities(verbose=True)
